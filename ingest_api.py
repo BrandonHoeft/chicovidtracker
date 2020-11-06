ea47883682fd96b6691588d25fbb0f709b531874
bhoeft <brandonmhoeft@gmail.com>
2020-11-05 23:29:51 -0600

    refactored config.ini into config.json. added API credentials.
    easier for me right now to work with json than configparser(). Complexity did not seem worth it. Created a run environment configuration for environment variables at runtime

--- ingest_api.py
+++ ingest_api.py
@@ -1,10 +1,18 @@
 import requests
-import configparser
+import json
 import pandas as pd
+import os
 
-parser = configparser.ConfigParser()
-parser.read('config.ini')
+# call API key for basic auth
+key_id = os.getenv('CHIDATAPORTAL_ID')
+key_secret = os.getenv('CHIDATAPORTAL_SECRET')
 
-response = requests.get(parser['api']['url'])
+# Load API call details
+with open('config.json') as js:
+    cfg = json.load(js)
+
+url = cfg['base'] + '?' + cfg['where'] + '&' + cfg['limit']
+
+response = requests.get(url, auth=(key_id, key_secret))
 
 df = pd.json_normalize(response.json())
