241c836feba2be23a42eb8c116642c98c23dbae6
bhoeft <brandonmhoeft@gmail.com>
2020-11-08 21:35:51 -0600

    wrapped get request into a function, get_api_json()

--- ingest_api.py
+++ ingest_api.py
@@ -14,6 +14,39 @@ with open('config.json') as js:
 
 url = cfg['baseurl'] + urlencode(cfg['qrystr'])
 
-api_response = requests.get(url, auth=(key_id, key_secret))
 
-df = pd.json_normalize(api_response.json())
+def get_api_json(target_url, api_key, api_secret):
+    """make get request to Socrata Chicago Data Portal API
+
+    Parameters
+    ----------
+    target_url : str
+        a validly formed URL string
+    api_key : str
+        API key id for HTTP basic auth
+    api_secret : str
+        API secret passcode for HTTP basic auth
+
+    Raises
+    ------
+    RequestException
+        base exception class for requests library
+
+    Returns
+    -------
+    list
+        if successful, a list of dicts is returned
+    """
+    api_response = requests.get(target_url, auth=(api_key, api_secret))
+
+    try:
+        api_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        # return errors from bad requests
+        return f"error: {e}"
+
+    # if a successful response status code
+    return api_response.json()
+
+# get api data, put into dataframe
+df = pd.json_normalize(get_api_json(url, key_id, key_secret))
