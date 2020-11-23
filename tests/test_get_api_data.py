066c5f282713cbc100aef014db8a2147f6cb2a64
bhoeft <brandonmhoeft@gmail.com>
2020-11-22 20:29:07 -0600

    added a test_get_api_json_1() test, which was skipped with pytest.mark.skip fixture

---
+++ tests/test_get_api_data.py
@@ -0,0 +1,6 @@
+import pytest
+from chicovidtracker.get_api_data import get_api_json
+
+@pytest.mark.skip(reason="get_api_json() test not yet fully built")
+def test_get_api_json_1():
+    print('Nothing to see here')
