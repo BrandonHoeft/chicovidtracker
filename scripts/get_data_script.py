ed60c723efeab2a28120b783fe45206a56022480
bhoeft <brandonmhoeft@gmail.com>
2020-11-22 20:46:43 -0600

    added small comment to get_data_script.py

--- scripts/get_data_script.py
+++ scripts/get_data_script.py
@@ -2,6 +2,7 @@ import pandas as pd
 from chicovidtracker.api_helpers import load_config, get_env_vars, build_url_from_config
 from chicovidtracker.get_api_data import get_api_json
 
+# Setup for an API call to Socrata Chicago Data Portal
 config = load_config("chicovidtracker/api_config.json")
 key_id, key_secret = get_env_vars(config['env_vars'])
 url = build_url_from_config(config["baseurl"], config["qrystr"])
