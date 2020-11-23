84c7634c546212214b77e7ed06cbd9ed6268c9d4
bhoeft <brandonmhoeft@gmail.com>
2020-11-22 21:53:04 -0600

    added a setup.py so that Travis CI server will hopefully install the chicovidtracker package where the __init__.py is located

---
+++ setup.py
@@ -0,0 +1,10 @@
+# for Travis CI to properly
+from setuptools import setup, find_packages
+find__init__ = find_packages()
+
+options = dict(name="chicovidtracker",
+               author="Brandon Hoeft",
+               packages=find__init__)
+
+if __name__ == '__main__':
+    setup(**options)
